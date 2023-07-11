86f6baf3882bcf10cad7071d41622b3e99ff8195
quangvinh1102 <vinhls2002@gmail.com>
2023-07-11 21:19:10 +0700

    fix controler

--- app/Http/Controllers/Admin/UserAdminController.php
+++ app/Http/Controllers/Admin/UserAdminController.php
@@ -99,33 +99,5 @@ class UserAdminController extends Controller
         return response()->json(['mess' => 'Đăng ký thành công!!']);
     }
 
-    public function updateAdmin(Request $request, $id)
-    {
-        $user = User::find($id);
-        try {
-            $user->update([
-                "fullname" => $request->fullname,
-                "email" => $request->email,
-                "phone" => $request->phone,
-                "gender" => $request->gender,
-                "birthday" => Carbon::parse($request->birthday)->format('Y/m/d'),
-            ]);
-        } catch (\Throwable $th) {
-            dd($th);
-        }
-    }
-
-    public function storeAdmin(RegisterAdminRequest $request)
-    {
-        $create = Admin::create([
-            "fullname" => $request->fullname,
-            "avatar" => 'test',
-            "email" => $request->email,
-            "password" => bcrypt($request->password),
-            "phone" => $request->phone,
-            "gender" => $request->gender,
-            "birthday" => Carbon::parse($request->birthday)->format('Y/m/d'),
-        ]);
-        return response()->json(['mess' => 'Thêm tài khoản thành công!!']);
-    }
+    
 }
