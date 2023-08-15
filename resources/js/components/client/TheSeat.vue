cc8febcadcd964912ca81896850931c5cb68a334
Hoa1210 <luonghoa121002@gmail.com>
2023-08-16 00:07:37 +0700

    fix css

--- resources/js/components/client/TheSeat.vue
+++ resources/js/components/client/TheSeat.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="seat col-lg-8 col-12">
-    <h4 class="text-light bg-secondary text-center">Theo quy định của cục điện ảnh, phim này không dành cho khán giả
-      dưới 13
-      tuổi.</h4>
+    <h4 class="color-changing-bg mb-5">Theo quy định của cục điện ảnh, phim này không dành cho khán giả dưới 13 tuổi.</h4>
     <div id="seat my-3">
       <div class="row">
         <div class="seat-def col-1 d-flex flex-column" v-for="seat in seats" :key="seat.id" v-bind:id="seat.id"
           @click="handleSeat(seat.id)">
-          <i class="fa-solid fa-chair fs-4" v-if="seat.type_seat == 'thuong'"></i>
-          <i class="fa-solid fa-couch fs-5" v-else></i>
+          <i class="fa-solid fa-chair fs-4 " v-if="seat.type_seat == 'thuong'"></i>
+          <i class="fa-solid fa-couch fs-5 " v-else></i>
           <span>{{ seat.name }}</span>
         </div>
       </div>
     </div>
   </div>
-  <div class="movie-info col-lg-4 col-12">
+  <div class="movie-info col-lg-4 col-12 p-5">
     <div class="row">
       <div class="col-6"><img :src="movieBooking.img" alt="" style="width: 100%;">
       </div>
       <div class="col-6">
-        <h2>{{ movieBooking.name }}</h2>
-        <h5 class="mt-5">2D Phụ Đề</h5>
+        <h2 class="color-default">{{ movieBooking.name }}</h2>
+        <h5 class="mt-3">2D Phụ Đề</h5>
       </div>
     </div>
     <div class="row mt-3">
@@ -38,9 +36,9 @@
     </div>
     <div class="row mt-3">
       <div class="col-6"><i class="fa-regular fa-calendar"></i> Ngày Chiếu</div>
-      <div class="col-6" >
+      <div class="col-6">
         <template v-for="schedule in movieBooking.schedules" :key="schedule.id">
-          <span v-if="schedule.id == timeId ">{{ schedule.date }}</span>
+          <span v-if="schedule.id == timeId">{{ schedule.date }}</span>
         </template>
       </div>
     </div>
@@ -48,7 +46,7 @@
       <div class="col-6"><i class="fa-regular fa-clock"></i> Giờ Chiếu</div>
       <div class="col-6">
         <template v-for="schedule in movieBooking.schedules" :key="schedule.id">
-          <span v-if="schedule.id == timeId ">{{ schedule.time }}</span>
+          <span v-if="schedule.id == timeId">{{ schedule.time }}</span>
         </template>
       </div>
     </div>
@@ -56,25 +54,23 @@
       <div class="col-6"><i class="fa fa-desktop"></i> Phòng Chiếu</div>
       <div class="col-6">
         <template v-for="schedule in movieBooking.schedules" :key="schedule.id">
-          <span v-if="schedule.id == timeId ">{{ schedule.room.name }}</span>
+          <span v-if="schedule.id == timeId">{{ schedule.room.name }}</span>
         </template>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col-6"><i class="fa fa-cubes"></i> Ghế Ngồi</div>
-      <div class="col-6" id="seatClick"><span>{{ seatClick }}</span></div>
+      <div class="col-6" id="seatClick"><template v-for="(value, index) in listSeat" :key="index"><template v-if="index > 0">,</template>{{ " " + value.value }}</template></div>
     </div>
     <div class="mt-3 text-center">
-      <button type="button" class="btn btn-primary">Mua vé</button>
+      <a href="#" class="btn btn-2 text-white fs-5 w-50"> Tiếp tục</a>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from 'vue';
+import { ref } from 'vue';
 import axios from 'axios';
-
-import dayjs from 'dayjs';
 import { useRoute } from 'vue-router';
 export default {
   props: {
@@ -84,26 +80,30 @@ export default {
     const route = useRoute()
     const timeId = route.params.timeId
     const seats = ref([])
+    const listSeat = ref([]);
     const getSeats = () => axios.get('/api/client/movie/seat')
       .then((reponse) => {
         seats.value = reponse.data.listSeat;
       })
-    getSeats()
+      getSeats()
     const handleSeat = (id) => {
-      const seats = document.getElementById('seatClick')
-      const clickedSeat = document.getElementById(id).textContent
-      if (seats.textContent.includes(clickedSeat)) {
-        seats.textContent = seats.textContent.replace(clickedSeat, " ");
-      }else {
-        seats.textContent += clickedSeat + ' '
-      }
-      document.getElementById(id).classList.toggle('bg-primary')
-    }
-    
-   
+      const clickedSeat = document.getElementById(id);
+      const index = listSeat.value.findIndex(item => item.key == id); 
+      
+      if (index != -1) {
+        listSeat.value.splice(index, 1);
+      } else {
+        listSeat.value.push({ key: id, value: clickedSeat.textContent })
+      } 
+      clickedSeat.querySelector('i').classList.toggle('color-default')
+      clickedSeat.querySelector('span').classList.toggle('color-default')
+    } 
+
+
     return {
       seats,
       timeId,
+      listSeat,
       handleSeat,
     }
   }
--- resources/js/components/client/TheShowtime.vue
+++ resources/js/components/client/TheShowtime.vue
@@ -1,156 +1,13 @@
 <template>
     <div class="container mt-5">
-        <!-- <nav>
-            <div class="nav nav-tabs d-flex justify-content-center" id="nav-tab" role="tablist" >
-                <template v-for="schedule in movie" :key="schedule.value">
-                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" v-bind:data-bs-target="`nav-day-${schedule.value}`"
-                        type="button" role="tab" v-bind:aria-controls="`nav-day-${schedule.value}`" aria-selected="true" >
-                        <h3 class="color-default">{{ schedule.label }}</h3>
-                    </button>
-                </template>
-            </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-day-" role="tabpanel" aria-labelledby="nav-home-tab">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">131 ghế trống</div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="nav-day-9" role="tabpanel" aria-labelledby="nav-profile-tab">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="nav-day-3" role="tabpanel" aria-labelledby="nav-123-tab">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mt-3">
-                        <div class="time-box">
-                            <div class="time p-3 text-center bg-default text-white">
-                                16:35
-                            </div>
-                            <div class="table mt-2 text-center">
-                                131 ghế trống
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
-        <a-tabs centered v-model:activeKey="activeKey">
+        <a-tabs centered >
             <template v-for="(schedule, index) in arrayTransformation(movie.schedules)" :key="index">
                 <a-tab-pane :tab="schedule.date" force-render>
-                    <div class="row align-items-center justify-content-center">
-                        <div v-for="(time, index) in schedule.time" :key="index" class="col mt-3">
+                    <div class="row ">
+                        <div v-for="(time, index) in schedule.time" :key="index" class="col-2 mt-3">
                             <div class="time-box">
-                                <div class="time p-3 text-center bg-default text-white">
-                                    <router-link :to="{ name: 'book-ticket', params: { id: schedule.slug, timeId: time.id } }">{{ time.time
+                                <div class="time p-3 text-center bg-default">
+                                    <router-link :to="{ name: 'book-ticket', params: { id: schedule.slug, timeId: time.id } }" class="text-white">{{ time.time
                                     }}</router-link>
                                 </div>
                                 <div class="table mt-2 text-center">131 ghế trống</div>
@@ -164,9 +21,7 @@
     </div>
 </template>
 
-<script>
-import { toRefs, ref, reactive } from 'vue'
-import dayjs from 'dayjs';
+<script> 
 export default {
     props: {
         movie: Object
